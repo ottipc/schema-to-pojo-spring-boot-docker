b73e44393546a9d56ce2fc7f006c1a0586330452
Ottavio Braun <ottavio.braun@googlemail.com>
2023-10-10 13:35:03 +0200

    MOD adding new src files and application.properties

--- src/main/java/com/devguy/shematoclassdefintion/Runner.java
+++ src/main/java/com/devguy/shematoclassdefintion/Runner.java
@@ -2,23 +2,14 @@ package com.devguy.shematoclassdefintion;
 
 import com.devguy.jsonschematojava.Composition;
 import com.devguy.jsonschematojava.Files;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Runner {
 
-    final static ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-    final static Logger LOGGER = LoggerFactory.getLogger(Runner.class);
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws JsonProcessingException {
-
-        LOGGER.info("********************** STARTUO JSON TO POJO **************************");
         final Composition composition1 = new Composition();
         composition1.setMaterial(Composition.Material.BAMBOO);
         composition1.setPercentage(7.00);
@@ -30,8 +21,6 @@ public class Runner {
         composition2.setAdditionalProperty("name", "pumuckl");
 
         List<Composition> compositions = Arrays.asList(composition1, composition2);
-        LOGGER.debug("Compositions:");
-        LOGGER.debug(ow.writeValueAsString(compositions));
 
         final Files files = new Files();
         files.setName("Oktberfest");
@@ -41,9 +30,6 @@ public class Runner {
         files.setComposition(compositions);
         files.setVendorBusinessModel(Files.VendorBusinessModel.TRADER);
         files.setAdditionalProperty("nachname", "Braun");
-
-        LOGGER.debug("Files:");
-        LOGGER.debug(ow.writeValueAsString(files));
 
 
 
--- src/main/java/com/devguy/shematoclassdefintion/ShemaToClassdefintionApplication.java
+++ src/main/java/com/devguy/shematoclassdefintion/ShemaToClassdefintionApplication.java
@@ -1,8 +1,5 @@
 package com.devguy.shematoclassdefintion;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +10,8 @@ import org.slf4j.LoggerFactory;
 @RestController
 public class ShemaToClassdefintionApplication {
 
-    Messages messages = new Messages();
+
     private static Logger LOGGER = LoggerFactory.getLogger(ShemaToClassdefintionApplication.class);
-    final static ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 
     @GetMapping("/message")
     public String getMessage() {
@@ -23,17 +19,6 @@ public class ShemaToClassdefintionApplication {
         return "Schema to Pojo is Running";
     }
 
-    @GetMapping("/files")
-    public String getFiles() throws JsonProcessingException {
-        LOGGER.info("Fetching files...");
-        return ow.writeValueAsString(messages.getFiles());
-    }
-
-    @GetMapping("/compositions")
-    public String getCompositions() throws JsonProcessingException {
-        LOGGER.info("Fetching compositions...");
-        return ow.writeValueAsString(messages.getAllCompositions());
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShemaToClassdefintionApplication.class, args);
