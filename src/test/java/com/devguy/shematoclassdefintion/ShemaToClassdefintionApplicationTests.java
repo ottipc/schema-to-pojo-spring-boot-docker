8c8f5319fd41f41f2919331277b7a2ef178c25e5
Ottavio Braun <ottavio.braun@googlemail.com>
2023-10-10 03:03:02 +0200

    ADD Test Application

---
+++ src/test/java/com/devguy/shematoclassdefintion/ShemaToClassdefintionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devguy.shematoclassdefintion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShemaToClassdefintionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
